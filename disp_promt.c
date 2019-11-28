59213ba02094a0be630eb17b40ebc60b0904b776
Edward Osorno <edward-1094@hotmail.com>
2019-11-28 06:10:20 +0000

    functions

--- disp_promt.c
+++ disp_promt.c
@@ -1,51 +1,14 @@
-#define _GNU_SOURCE
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <unistd.h>
 #include "shell.h"
-
 
 int main()
 {
-	char *keywords = NULL, *machete = "/";
+	char *keywords = NULL;
 	size_t buf = 0;
-	int c2 = 0, c = 0, i = 0;
-	int check = 1, check2 = 1, aux=1;
+	int c2 = 0, c = 0;
+	int check = 1;
 	char **argv = NULL, **paty = NULL;
 
-	while (strtok(environ[i], "=:") && check2)
-	{
-		if (!_strncmp(environ[i], "PATH", 4))
-		{
-			paty = malloc(8);
-			if (!paty)
-			{
-				write(1, "error1", 6);
-				return (-1);
-			}
-			paty[c2] = strtok(NULL, "=:");
-
-			while (paty[c2])
-			{
-				c2++;
-				paty = realloc(paty, 8 * (c2 + 1));
-				if (!paty)
-				{
-				 	write(1, "error2", 6);
-				 	return (-1);
-				}
-
-				paty[c2] = strtok(NULL, "=:");
-			}
-		check2 = 0;
-		}
-		i++;
-	}
-
-
+	paty = path(&c2);
 
 	while (check)
 	{
@@ -74,50 +37,8 @@ int main()
 
 		}
 
+		execute(argv, paty, c2, check);
 
-
-		if (access(argv[0], F_OK) && check)
-		{
-			i = 0;
-
-			while(i <= c2)
-			{
-				machete = _str_concat(paty[i], argv[0]);
-				if (access(machete, F_OK) && check)
-				{
-					aux = 0;
-				}
-				else
-				{
-					if (fork()==0)
-					{
-						execve(machete, argv, environ);
-					}
-					else
-					{
-						wait(NULL);
-						aux = 1;
-						i = c2 + 10;
-					}
-				}
-				i++;
-				free(machete);
-
-			}
-			if(!aux)
-				write(1, "command not found\n", 18);
-		}
-		else
-		{
-			if (fork()==0)
-			{
-				execve(argv[0], argv, environ);
-			}
-			else
-			{
-				wait(NULL);
-			}
-		}
 		free(argv);
 	}
 	free(keywords);
--- shell.h
+++ shell.h
@@ -1,5 +1,13 @@
 #ifndef SHELL
 #define SHELL
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+
 
 /**
  * struct argv - singly linked list
@@ -17,6 +25,8 @@ typedef struct arg
 int _strncmp(char *s1, char *s2, int n);
 int _strcmp(char *s1, char *s2);
 char *_str_concat(char *s1, char *s2);
-
+char **path(int *c2);
+int execute(char **argv, char **paty, int c2, int check);
+char *_strdup(char *str);
 
 #endif
